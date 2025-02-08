8ae0cde4542d7146cda015484448e4df62f7e145
Giulia <giuliaarmanelli24@gmail.com>
2025-02-08 16:39:20 -0300

    Adicionar event listener para a tecla Enter para acionar a adição de um amigo

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+document.getElementById("amigo").addEventListener("keydown", apertarTeclaEnter);
+
+function apertarTeclaEnter(evento){
+    if (evento.key === "Enter") {
+        adicionarAmigo();
+    }
+}
